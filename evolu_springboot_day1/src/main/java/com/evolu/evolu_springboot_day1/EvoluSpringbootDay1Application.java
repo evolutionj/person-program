c8b420bcc7f717aa94968a9f2f2c0e17e63649d0
archon guo <evolutionj2016@gmail.com>
2020-06-18 17:57:39 +0800

    [add] study day1

---
+++ evolu_springboot_day1/src/main/java/com/evolu/evolu_springboot_day1/EvoluSpringbootDay1Application.java
@@ -0,0 +1,13 @@
+package com.evolu.evolu_springboot_day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoluSpringbootDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoluSpringbootDay1Application.class, args);
+    }
+
+}
