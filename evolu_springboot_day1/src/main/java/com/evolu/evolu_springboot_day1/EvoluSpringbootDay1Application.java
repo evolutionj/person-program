8183abad73779a3673241d25dd8fbb39175c9b05
archon guo <evolutionj2016@gmail.com>
2020-06-30 15:18:57 +0800

    [update] study day1

--- evolu_springboot_day1/src/main/java/com/evolu/evolu_springboot_day1/EvoluSpringbootDay1Application.java
+++ evolu_springboot_day1/src/main/java/com/evolu/evolu_springboot_day1/EvoluSpringbootDay1Application.java
@@ -1,12 +1,23 @@
 package com.evolu.evolu_springboot_day1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author guoqi7
+ */
 @SpringBootApplication
 public class EvoluSpringbootDay1Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EvoluSpringbootDay1Application.class);
+
     public static void main(String[] args) {
+        LOGGER.error("aaaaaaaaaaaa");
+        LOGGER.warn("bbbbbbbbbbb");
+        LOGGER.trace("cccccccccccc");
         SpringApplication.run(EvoluSpringbootDay1Application.class, args);
     }
 
